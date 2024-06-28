64e444abc2fa0f1264f01986465e9a72a68cb606
ara fa adri <arafa.sco@gmail.com>
2024-06-28 15:00:47 +0700

    update table

--- src/components/Table/EmployeForAbsenTable.tsx
+++ src/components/Table/EmployeForAbsenTable.tsx
@@ -15,15 +15,16 @@ const EmployeForAbsenTable = (props : any) => {
           <div className="col-span-12 xl:col-span-12 2xl:col-span-12">
               <div className="flex flex-col-reverse px-5 py-4 border-b sm:flex-row text-slate-500 border-slate-200/60">
                 <div className="flex items-center justify-end sm:ml-auto">
-                  <div>
+                  <div className='flex items-center mx-4'>
                     <FormInput
                       type="text"
-                      className="block px-1 py-0 mt-0 w-10 mx-4 text-center"
+                      className="block px-1 py-0 mt-0 w-10 mx-2 text-center text-xs"
                       placeholder="0"
                       name='limit'
-                      value={limit}
+                      value={datas.count === 0 ? 0 : (limit > datas.count ? datas.count : limit)}
                       onChange={(e)=>setLimit(e.target.value)}
-                    />
+                    /> 
+                    <p className=' text-center text-xs'>/ {datas.count}</p>
                   </div>
                   <div className="text-xs">{page <= allPage ? page : allPage} of {allPage} page </div>
                   <div className="flex items-center justify-center w-5 h-5 ml-5" >
